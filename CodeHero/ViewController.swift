39bdea8afc051ac2e0645c691a2e1d5980574622
Lucas Bordini <lucas.bordini@hotmail.com>
2020-03-11 14:24:34 -0300

    Initial Commit

---
+++ CodeHero/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeHero
+//
+//  Created by Lucas Bordini Ribeiro de Araujo on 11/03/20.
+//  Copyright © 2020 Lucas Bordini Ribeiro de Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodeHeroTests/CodeHeroTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodeHeroTests.swift
+//  CodeHeroTests
+//
+//  Created by Lucas Bordini Ribeiro de Araujo on 11/03/20.
+//  Copyright © 2020 Lucas Bordini Ribeiro de Araujo. All rights reserved.
+//
+
+import XCTest
+@testable import CodeHero
+
+class CodeHeroTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
